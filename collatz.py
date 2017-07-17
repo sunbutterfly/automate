2ce073e97cacc512fa741283954ae7ccfec557c2
Laura Sun <sunbutterfly@mac.com>
2017-07-16 18:37:57 -0700

    code compress

--- collatz.py
+++ collatz.py
@@ -8,8 +8,7 @@ def collatz(num):
         return((3*int(num))+1)
 
 User_input = input('Enter Number:')
-User_input = int(User_input)
-New_num = User_input
+New_num = int(User_input) 
 
 while True:
     New_num = collatz(New_num)
