b906c55d4e4422c5f494820612f6d472fd38d18b
Laura Sun <sunbutterfly@mac.com>
2017-07-16 18:05:41 -0700

    Collatz-Final

--- collatz.py
+++ collatz.py
@@ -3,11 +3,18 @@ def collatz(num):
     even_number = (num % 2 ==0)
     odd_number = (num % 2==1)
     if even_number:
-        print(int(num)//2)
+        return (int(num)//2)
     else: 
-        print((3*int(num))+1)
+        return((3*int(num))+1)
 
 User_input = input('Enter Number:')
 User_input = int(User_input)
-collatz(User_input)
 
+while True:
+    if collatz(User_input) <=1:
+        print(collatz(User_input))
+        break
+    else:
+        User_input = collatz(User_input)
+        print(User_input)
+        
