065799144446f6dccf73a02f052716b209be726a
Laura Sun <sunbutterfly@mac.com>
2017-07-23 11:09:53 -0700

    chapter 5--fantasy game--final version

--- fantasy_game _inventory.py
+++ fantasy_game _inventory.py
@@ -1,6 +1,17 @@
 
 
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+def addToInventory(inventory, addedItems):
+    # structure: "for varname in sequence(sequence can be a list or tuple):"
+    # wrong: "for inventory in addToInventory:"
+    #inventory is dictionary
+    #addedItems is a list of string
+    #addedItem is a string
+    for addedItem in addedItems:
+        inventory.setdefault(addedItem,0)
+        inventory[addedItem] = inventory[addedItem]+1
+    return inventory
+
+
 
 def displayInventory(inventory):
     print("Inventory:")
@@ -8,10 +19,20 @@ def displayInventory(inventory):
     for k,v in inventory.items():
         print(str(v) + ' '+ str(k))
         item_total = item_total + v
-
-
     print("Total number of items: "+ str(item_total))
-displayInventory(stuff)
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger','gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
+
+
+
+
+
+
+
 
 
 
