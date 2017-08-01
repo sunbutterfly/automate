46e780c8875d292ed677581085a33edf466ab591
Laura Sun <sunbutterfly@mac.com>
2017-07-31 20:17:38 -0700

    divisor pratice file

---
+++ divisor.py
@@ -0,0 +1,22 @@
+
+###Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+###(If you don’t know what a divisor is, it is a number that divides evenly into another number.
+###For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+
+userNum = int(input('please enter a number:'))
+
+def numDivisor(num):
+    numList = []
+    for num in numList:
+        if userNum % num == 0:
+            newNum = userNum
+
+            numList.append(num)
+
+
+
+
+print(numDivisor(userNum))
+
+
